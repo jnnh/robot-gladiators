2939f94d3719451770c028ca0bf9fb01db1d60b6
jnnh <jenniferhorvath@cmail.carleton.ca>
2021-12-01 14:22:01 -0500

    fight function added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert ("this is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+
+
+function fight() {
+    window.alert("the fight has begun!");
+}
+
+// fight();
